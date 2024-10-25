b14987d230e39eb7ca5a5e2e319d9e18cb29583e
roxanne <manuscript122333@gmail.com>
2024-10-25 19:54:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-
-// Enable CORS for all routes
-app.use(cors({ origin: 'https://onsen.onrender.com' } ));
-
-
-
-app.get('/', (req, res) => {
-    res.send('CORS is now enabled for all routes');
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 
 
 
