b970b2f1d1da05ecdc17d9e9616f02679dfba836
roxanne <manuscript122333@gmail.com>
2024-10-26 22:52:49 +0530

    Delete public directory

--- public/fullscreen.js
+++
@@ -1,4 +0,0 @@
-addEventListener("click", (function() {
-    var e = document.documentElement;
-    (e.requestFullScreen || e.webkitRequestFullScreen || e.mozRequestFullScreen).call(e)
-}));
--- public/open.php
+++
@@ -1,1 +0,0 @@
-No Content: https://kotmirikokl.top/mtk/16476e2fca9659bd41cca36ea6d8d09b/open.php?ecode=2hjb72taa0g4
